902394f876f58db8114e5bd8c8d8cc37b6863181
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-01-25 16:02:18 +0700

    nesting stories

--- tic-tac-toe/.storybook/config.js
+++ tic-tac-toe/.storybook/config.js
@@ -1,4 +1,12 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+
+addDecorator(story => (
+  <div style={{textAlign: 'center'}}>
+    {story()}
+  </div>
+));
+
 
 const req = require.context('../src/components', true, /\.stories\.js$/)
 
--- tic-tac-toe/src/components/Button.stories.js
+++ tic-tac-toe/src/components/Button.stories.js
@@ -3,10 +3,12 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Button from './Button';
 
-storiesOf('Button', module)
+storiesOf('My App/Buttons/Simple', module)
   .add('with text', () => (
     <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
+  ));
+  
+storiesOf('My App/Buttons/Emoji', module)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
   ));
